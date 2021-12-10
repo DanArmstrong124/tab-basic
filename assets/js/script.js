0c4f5bf966a8766063081b7ec7113465064ed626
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-12-10 17:16:53 +0000

    Test

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,5 @@ var openfri = "10am -> 1am";
 var opensat = "11am -> 12am";
 var opensun = "2pm -> 7:30pm";
 
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-
-today = mm + '/' + dd + '/' + yyyy;
-
-alert(today.getDay());
+const d = new Date();
+let day = d.getDay();
